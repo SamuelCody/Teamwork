968cd7f68efac7b7fdcb8830eb208926f0282048
Ayinde Samuel <ayindesamuel0@gmail.com>
2020-06-12 20:57:05 +0100

    Set up patch /articles/articleId endpoint

--- handlers/articles.js
+++ handlers/articles.js
@@ -39,3 +39,41 @@ exports.createArticle = async function (req, res, next) {
     });
   }
 };
+
+//edit an article
+exports.editArticle = async function (req, res, next) {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, process.env.SECRET_KEY, async function (err, decoded) {
+      let article = await Article.findById(req.params.articleId);
+      if (decoded.id == article.user) {
+        let editedArticle = await Article.findOneAndUpdate(
+          { _id: req.params.articleId },
+          { $set: { title: req.body.title, article: req.body.article } },
+          { new: true }
+        );
+        return res.status(200).json({
+          status: "success",
+          data: {
+            message: "Article successfully updated",
+            articleId: editedArticle._id,
+            createdOn: editedArticle.createdAt,
+            title: editedArticle.title,
+            article: editedArticle.article,
+            userId: editedArticle.user,
+          },
+        });
+      } else {
+        return next({
+          status: 401,
+          message: "User Unauthorized",
+        });
+      }
+    });
+  } catch (err) {
+    return next({
+      status: 401,
+      message: "User Unauthorized",
+    });
+  }
+};
